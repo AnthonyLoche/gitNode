7440310ac31e7f316c777f663762c5374e5e792c
AnthonyLoche <anthonylocheifc@gmail.com>
2024-07-02 19:40:40 -0300

    cleide

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET} = process.env;
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    callbackURL: "https://git-node.vercel.app/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
