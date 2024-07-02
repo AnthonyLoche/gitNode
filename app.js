ba892023c7c39297f059914218d898272be726b5
AnthonyLoche <anthonylocheifc@gmail.com>
2024-07-02 20:37:56 -0300

    asdasddsadas

--- app.js
+++ app.js
@@ -1,68 +1,62 @@
-const express = require('express');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
-const session = require('express-session');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const app = express();
-app.use(cors());
+import express from 'express';
+import axios from 'axios';
+import session from 'express-session';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-const {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET} = process.env;
+const app = express();
+app.use(cors({ origin: 'http://localhost:5173/', credentials: true }));
 
-// Configuração do Passport
-passport.use(new GitHubStrategy({
-    clientID: GITHUB_CLIENT_ID,
-    clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "https://git-node.vercel.app/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log(profile);
-    console.log('accessToken:', accessToken);
-    return done(null, profile);
-  }
-));
+app.use(session({
+  secret: 'YOUR_SECRET_KEY',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, httpOnly: true } // Secure should be true in production with HTTPS
+}));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const REDIRECT_URL = process.env.REDIRECT_URL;
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+app.get('/auth/github', (req, res) => {
+    const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URL}&scope=user`;
+    res.redirect(redirectUrl);
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+app.get('/auth/github/callback', async (req, res) => {
+    const code = req.query.code;
+    const tokenResponse = await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            code: code,
+            redirect_url: REDIRECT_URL,
+        },
+        { headers: { accept: 'application/json' } }
+    );
+    const accessToken = tokenResponse.data.access_token;
 
-// Configuração do Express
-app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Rotas de Login
-app.get('/auth/github', (req, res, next) => {
-    // console.log(req.body)
-    // const login = passport.authenticate('github', { scope: ['user:email'] })(req, res, next);
-
-    // console.log(login)
-
-    const url = `https://git-node.vercel.app`
-
-    const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_url=${url}&scope=user`;
-    res.redirect(redirectUrl)
-
-});
-  
-  app.get('/auth/github/callback', 
-
-        passport.authenticate('github', { failureRedirect: '/' }),
-    function(req, res) {
-    
-      res.redirect('/');
+    const userResponse = await axios.get('https://api.github.com/user', {
+        headers: { Authorization: Bearer `${accessToken}` },
     });
 
-// Inicie o servidor
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+    // Save user data in session
+    req.session.user = userResponse.data;
+    res.redirect('http://localhost:5173'); // Redirect back to the frontend
+});
+
+app.get('/api/user', (req, res) => {
+    if (req.session.user) {
+        res.json({ user: req.session.user });
+    } else {
+        res.status(401).json({ error: 'User not authenticated' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
