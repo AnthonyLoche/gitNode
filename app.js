b636ea14e0a89b2495e361e5c14e6edb64522650
AnthonyLoche <anthonylocheifc@gmail.com>
2024-07-02 21:00:24 -0300

    skibidi sigma

--- app.js
+++ app.js
@@ -21,42 +21,44 @@ const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const REDIRECT_URL = process.env.REDIRECT_URL;
 
 app.get('/auth/github', (req, res) => {
-    const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URL}&scope=user`;
-    res.redirect(redirectUrl);
+  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URL}&scope=user`;
+  res.redirect(redirectUrl);
 });
 
 app.get('/auth/github/callback', async (req, res) => {
-    const code = req.query.code;
-    const tokenResponse = await axios.post(
-        'https://github.com/login/oauth/access_token',
-        {
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            code: code,
-            redirect_url: REDIRECT_URL,
-        },
-        { headers: { accept: 'application/json' } }
-    );
-    const accessToken = tokenResponse.data.access_token;
+  const code = req.query.code;
+  const tokenResponse = await axios.post(
+    'https://github.com/login/oauth/access_token',
+    {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code: code,
+      redirect_url: REDIRECT_URL,
+    },
+    { headers: { accept: 'application/json' } }
+  );
+  const accessToken = tokenResponse.data.access_token;
 
-    const userResponse = await axios.get('https://api.github.com/user', {
-        headers: { Authorization: `Bearer ${accessToken}` },
-    });
+  const userResponse = await axios.get('https://api.github.com/user', {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
 
-    // Save user data in session
-    req.session.user = userResponse.data;
-    res.redirect('https://vue-node-git.vercel.app/'); // Redirect back to the frontend
+  // Save user data in session
+  req.session.user = userResponse.data;
+  res.redirect('https://vue-node-git.vercel.app/'); // Redirect back to the frontend
 });
 
 app.get('/api/user', (req, res) => {
+  try {
     if (req.session.user) {
-        res.json({ user: req.session.user });
-    } else {
-        res.status(401).json({ error: 'User not authenticated' });
+      res.json({ user: req.session.user });
     }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
